df84b263f887d8d5179cfce86f7b57de168d0609
savkk <savkk@bk.ru>
2022-03-28 23:18:27 +0300

    add simple step title formatter

--- qase-api/src/main/java/io/qase/api/aspects/StepsAspects.java
+++ qase-api/src/main/java/io/qase/api/aspects/StepsAspects.java
@@ -11,6 +11,9 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Aspect
 public final class StepsAspects {
 
@@ -26,10 +29,29 @@ public final class StepsAspects {
     public void stepFinished(JoinPoint joinPoint) {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Step stepAnnotation = methodSignature.getMethod().getAnnotation(Step.class);
+        String stepsTitle = stepAnnotation.value();
+
+        stepsTitle = getTitle(joinPoint, stepsTitle);
+
         ResultCreateSteps step = new ResultCreateSteps()
-                .action(stepAnnotation.value())
+                .action(stepsTitle)
                 .status(ResultCreateSteps.StatusEnum.PASSED);
         StepStorage.addStep(step);
+    }
+
+    private String getTitle(JoinPoint joinPoint, String stepsTitle) {
+        String[] parameterNames = ((MethodSignature) joinPoint.getSignature()).getParameterNames();
+        Object[] args = joinPoint.getArgs();
+        Map<String, String> params = new HashMap<>();
+        for (int i = 0; i < parameterNames.length; i++) {
+            params.put(String.format("{%s}", parameterNames[i]), String.valueOf(args[i]));
+        }
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (stepsTitle.contains(entry.getKey())) {
+                stepsTitle = stepsTitle.replaceAll("\\" + entry.getKey(), entry.getValue());
+            }
+        }
+        return stepsTitle;
     }
 
     @AfterThrowing(pointcut = "method() && withStepAnnotation()", throwing = "e")
