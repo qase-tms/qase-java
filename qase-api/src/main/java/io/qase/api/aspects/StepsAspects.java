d358cfb97a6e56adf952f98dbba3160a05e6aa7b
Anatoliy Surin <savkk@bk.ru>
2022-04-06 07:45:15 +0300

    fixed work with array arguments in step formatter (#41)

--- qase-api/src/main/java/io/qase/api/aspects/StepsAspects.java
+++ qase-api/src/main/java/io/qase/api/aspects/StepsAspects.java
@@ -11,11 +11,14 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Aspect
 public final class StepsAspects {
+    private static final String DELIMITER = ", ";
 
     @Pointcut("@annotation(io.qase.api.annotation.Step)")
     public void withStepAnnotation() {
@@ -39,21 +42,6 @@ public final class StepsAspects {
         StepStorage.addStep(step);
     }
 
-    private String getTitle(JoinPoint joinPoint, String stepsTitle) {
-        String[] parameterNames = ((MethodSignature) joinPoint.getSignature()).getParameterNames();
-        Object[] args = joinPoint.getArgs();
-        Map<String, String> params = new HashMap<>();
-        for (int i = 0; i < parameterNames.length; i++) {
-            params.put(String.format("{%s}", parameterNames[i]), String.valueOf(args[i]));
-        }
-        for (Map.Entry<String, String> entry : params.entrySet()) {
-            if (stepsTitle.contains(entry.getKey())) {
-                stepsTitle = stepsTitle.replaceAll("\\" + entry.getKey(), entry.getValue());
-            }
-        }
-        return stepsTitle;
-    }
-
     @AfterThrowing(pointcut = "method() && withStepAnnotation()", throwing = "e")
     public void stepFailed(JoinPoint joinPoint, Throwable e) {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
@@ -63,5 +51,45 @@ public final class StepsAspects {
                 .status(ResultCreateSteps.StatusEnum.FAILED)
                 .addAttachmentsItem(IntegrationUtils.getStacktrace(e));
         StepStorage.addStep(step);
+    }
+
+    private static String getTitle(JoinPoint joinPoint, String stepsTitle) {
+        String[] parameterNames = ((MethodSignature) joinPoint.getSignature()).getParameterNames();
+        Object[] args = joinPoint.getArgs();
+        Map<String, String> params = new HashMap<>();
+        for (int i = 0; i < parameterNames.length; i++) {
+            params.put(String.format("{%s}", parameterNames[i]), getString(args[i]));
+        }
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (stepsTitle.contains(entry.getKey())) {
+                stepsTitle = stepsTitle.replaceAll("\\" + entry.getKey(), entry.getValue());
+            }
+        }
+        return stepsTitle;
+    }
+
+    private static String getString(Object args) {
+        if (args.getClass().isArray()) {
+            if (args instanceof int[]) {
+                return Arrays.toString((int[]) args);
+            } else if (args instanceof long[]) {
+                return Arrays.toString((long[]) args);
+            } else if (args instanceof double[]) {
+                return Arrays.toString((double[]) args);
+            } else if (args instanceof float[]) {
+                return Arrays.toString((float[]) args);
+            } else if (args instanceof boolean[]) {
+                return Arrays.toString((boolean[]) args);
+            } else if (args instanceof short[]) {
+                return Arrays.toString((short[]) args);
+            } else if (args instanceof char[]) {
+                return Arrays.toString((char[]) args);
+            } else if (args instanceof byte[]) {
+                return Arrays.toString((byte[]) args);
+            } else {
+                return Arrays.stream(((Object[]) args)).map(String::valueOf).collect(Collectors.joining(DELIMITER));
+            }
+        }
+        return String.valueOf(args);
     }
 }
