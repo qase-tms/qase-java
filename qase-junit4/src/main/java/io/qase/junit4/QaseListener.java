6397cb8ca7762ca48dde6e3e844a1e220bf74bba
Mykhailo Birintsev <mykhailo.birintsev@gmail.com>
2022-07-26 18:33:06 +0300

    [~] - logic of QaseReporter was reused for reducing of code duplication
    (cherry picked from commit 6e874ef9f04800d4cd11c26bfaec047e95aeff83)

--- qase-junit4/src/main/java/io/qase/junit4/QaseListener.java
+++ qase-junit4/src/main/java/io/qase/junit4/QaseListener.java
@@ -1,155 +1,73 @@
 package io.qase.junit4;
 
 
-import io.qase.api.QaseClient;
 import io.qase.api.StepStorage;
 import io.qase.api.annotation.CaseId;
 import io.qase.api.annotation.CaseTitle;
-import io.qase.api.exceptions.QaseException;
-import io.qase.client.ApiClient;
-import io.qase.client.api.AttachmentsApi;
-import io.qase.client.api.ResultsApi;
-import io.qase.client.api.RunsApi;
 import io.qase.client.model.ResultCreate;
 import io.qase.client.model.ResultCreate.StatusEnum;
-import io.qase.client.model.ResultCreateBulk;
 import io.qase.client.model.ResultCreateCase;
 import io.qase.client.model.ResultCreateSteps;
-import io.qase.client.services.ScreenshotsSender;
-import io.qase.client.services.impl.AttachmentsApiScreenshotsUploader;
+import io.qase.reporters.QaseReporter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.runner.Description;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
 
-import java.time.Duration;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Optional;
-import java.util.Set;
 
-import static io.qase.api.Constants.X_CLIENT_REPORTER;
-import static io.qase.api.QaseClient.getConfig;
 import static io.qase.api.utils.IntegrationUtils.getStacktrace;
 import static io.qase.client.model.ResultCreate.StatusEnum.*;
+import static io.qase.configuration.QaseModule.INJECTOR;
 
 @Slf4j
 public class QaseListener extends RunListener {
-    private static final ThreadLocal<Set<Integer>> cases = ThreadLocal.withInitial(HashSet::new);
-    private final ApiClient apiClient = QaseClient.getApiClient();
-    private final ResultsApi resultsApi = new ResultsApi(apiClient);
-    private final RunsApi runsApi = new RunsApi(apiClient);
-    private final ResultCreateBulk resultCreateBulk = new ResultCreateBulk();
 
-    private final ScreenshotsSender screenshotsSender =
-        new AttachmentsApiScreenshotsUploader(new AttachmentsApi(apiClient));
+    private static final String REPORTER_NAME = "JUnit 4";
 
-    private long startTime;
+    private final QaseReporter qaseReporter;
 
     public QaseListener() {
-        apiClient.addDefaultHeader(X_CLIENT_REPORTER, "JUnit 4");
+        qaseReporter = INJECTOR.getInstance(QaseReporter.class);
+        qaseReporter.setupReporterName(REPORTER_NAME);
     }
 
     @Override
     public void testStarted(Description description) {
-        startTime = System.currentTimeMillis();
-        cases.get().add(description.hashCode());
+        qaseReporter.onTestCaseStarted();
     }
 
     @Override
     public void testFinished(Description description) {
-        if (getConfig().useBulk()) {
-            addBulkResult(description, PASSED, null);
-        } else {
-            send(description, PASSED, null);
-        }
+        qaseReporter.onTestCaseFinished(getResultItem(description, PASSED, null));
     }
 
     @Override
     public void testFailure(Failure failure) {
-        if (getConfig().useBulk()) {
-            addBulkResult(failure.getDescription(), FAILED, failure.getException());
-        } else {
-            send(failure.getDescription(), FAILED, failure.getException());
-        }
+        qaseReporter.onTestCaseFinished(getResultItem(failure.getDescription(), FAILED, failure.getException()));
+        qaseReporter.onTestRunFinished();
     }
 
     @Override
     public void testAssumptionFailure(Failure failure) {
-        if (getConfig().useBulk()) {
-            addBulkResult(failure.getDescription(), SKIPPED, null);
-        } else {
-            send(failure.getDescription(), SKIPPED, null);
-        }
+        qaseReporter.onTestCaseFinished(getResultItem(failure.getDescription(), SKIPPED, null));
+        qaseReporter.onTestRunFinished();
     }
 
     @Override
     public void testIgnored(Description description) {
-        if (getConfig().useBulk()) {
-            addBulkResult(description, SKIPPED, null);
-        } else {
-            send(description, SKIPPED, null);
-        }
+        qaseReporter.onTestCaseFinished(getResultItem(description, SKIPPED, null));
+        qaseReporter.onTestRunFinished();
     }
 
     @Override
     public void testRunFinished(Result result) {
-        if (getConfig().useBulk()) {
-            sendBulkResult();
-        }
-        if (getConfig().runAutocomplete()) {
-            try {
-                runsApi.completeRun(getConfig().projectCode(), getConfig().runId());
-            } catch (QaseException e) {
-                log.error(e.getMessage());
-            }
-        }
-    }
-
-    private void addBulkResult(Description description, StatusEnum status, Throwable error) {
-        if (QaseClient.isEnabled() && cases.get().contains(description.hashCode())) {
-            resultCreateBulk.addResultsItem(getResultItem(description, status, error));
-            cases.get().remove(description.hashCode());
-        }
-    }
-
-    private void send(Description description, StatusEnum status, Throwable error) {
-        if (!QaseClient.isEnabled() || !cases.get().contains(description.hashCode())) {
-            return;
-        }
-        ResultCreate result = getResultItem(description, status, error);
-        try {
-            resultsApi.createResult(getConfig().projectCode(),
-                    getConfig().runId(),
-                    result);
-        } catch (QaseException e) {
-            log.error(e.getMessage());
-        }
-        cases.get().remove(description.hashCode());
-    }
-
-    private void sendBulkResult() {
-        if (!QaseClient.isEnabled()) {
-            return;
-        }
-        try {
-            resultsApi.createResultBulk(
-                    getConfig().projectCode(),
-                    getConfig().runId(),
-                    resultCreateBulk
-            );
-            screenshotsSender.sendScreenshotsIfPermitted();
-            resultCreateBulk.getResults().clear();
-        } catch (QaseException e) {
-            log.error(e.getMessage());
-        }
+        qaseReporter.onTestRunFinished();
     }
 
     private ResultCreate getResultItem(Description description, StatusEnum status, Throwable error) {
-        long end = System.currentTimeMillis();
-        Duration duration = Duration.ofMillis(end - startTime);
-
         Long caseId = getCaseId(description);
         String caseTitle = null;
         if (caseId == null) {
@@ -168,7 +86,6 @@ public class QaseListener extends RunListener {
                 ._case(caseTitle == null ? null : new ResultCreateCase().title(caseTitle))
                 .caseId(caseId)
                 .status(status)
-                .timeMs(duration.toMillis())
                 .comment(comment)
                 .stacktrace(stacktrace)
                 .steps(steps.isEmpty() ? null : steps)
