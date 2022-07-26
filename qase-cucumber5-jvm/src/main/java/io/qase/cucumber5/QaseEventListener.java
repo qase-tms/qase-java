5ea5b7747104f67e1eee8355463cd90a49eb87ba
Mykhailo Birintsev <mykhailo.birintsev@gmail.com>
2022-07-26 16:04:07 +0300

    [~] - code duplication reduced (QaseReporter is reused)

--- qase-cucumber5-jvm/src/main/java/io/qase/cucumber5/QaseEventListener.java
+++ qase-cucumber5-jvm/src/main/java/io/qase/cucumber5/QaseEventListener.java
@@ -4,107 +4,71 @@ import io.cucumber.plugin.ConcurrentEventListener;
 import io.cucumber.plugin.event.*;
 import io.qase.api.QaseClient;
 import io.qase.api.StepStorage;
-import io.qase.api.exceptions.QaseException;
 import io.qase.api.utils.CucumberUtils;
-import io.qase.client.ApiClient;
-import io.qase.client.api.AttachmentsApi;
-import io.qase.client.api.ResultsApi;
-import io.qase.client.api.RunsApi;
 import io.qase.client.model.ResultCreate;
 import io.qase.client.model.ResultCreate.StatusEnum;
-import io.qase.client.model.ResultCreateBulk;
 import io.qase.client.model.ResultCreateSteps;
-import io.qase.client.services.ScreenshotsSender;
-import io.qase.client.services.impl.AttachmentsApiScreenshotsUploader;
-import io.qase.client.services.impl.NoOpScreenshotsSender;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.qase.reporters.QaseReporter;
 
-import java.time.Duration;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
-import static io.qase.api.Constants.X_CLIENT_REPORTER;
-import static io.qase.api.QaseClient.getConfig;
 import static io.qase.api.utils.IntegrationUtils.getStacktrace;
+import static io.qase.configuration.QaseModule.INJECTOR;
 
 public class QaseEventListener implements ConcurrentEventListener {
-    private static final Logger logger = LoggerFactory.getLogger(QaseEventListener.class);
-    private final ThreadLocal<Long> startTime = new ThreadLocal<>();
-    private final ResultCreateBulk resultCreateBulk = new ResultCreateBulk();
-    private ResultsApi resultsApi;
-    private RunsApi runsApi;
 
-    private final ScreenshotsSender screenshotsSender;
+    private static final String REPORTER_NAME = "Cucumber 5-JVM";
+
+    private final QaseReporter qaseReporter;
 
     public QaseEventListener() {
+        this.qaseReporter = INJECTOR.getInstance(QaseReporter.class);
+        qaseReporter.setupReporterName(REPORTER_NAME);
+    }
+
+    @Override
+    public void setEventPublisher(EventPublisher publisher) {
         if (QaseClient.isEnabled()) {
-            ApiClient apiClient = QaseClient.getApiClient();
-            resultsApi = new ResultsApi(apiClient);
-            runsApi = new RunsApi(apiClient);
-            apiClient.addDefaultHeader(X_CLIENT_REPORTER, "Cucumber 5-JVM");
-            screenshotsSender = new AttachmentsApiScreenshotsUploader(new AttachmentsApi(apiClient));
-        } else {
-            screenshotsSender = new NoOpScreenshotsSender();
+            publisher.registerHandlerFor(TestCaseStarted.class, this::testCaseStarted);
+            publisher.registerHandlerFor(TestCaseFinished.class, this::testCaseFinished);
+            publisher.registerHandlerFor(TestRunFinished.class, this::testRunFinished);
         }
+    }
+
+    private void testRunFinished(TestRunFinished testRunFinished) {
+        qaseReporter.onTestRunFinished();
     }
 
     private void testCaseStarted(TestCaseStarted event) {
-        startTime.set(System.currentTimeMillis());
+        qaseReporter.onTestCaseStarted();
     }
 
     private void testCaseFinished(TestCaseFinished event) {
-        try {
-            Duration duration = Duration.ofMillis(System.currentTimeMillis() - startTime.get());
-            List<String> tags = event.getTestCase().getTags();
-            Long caseId = CucumberUtils.getCaseId(tags);
-            if (caseId == null) {
-                return;
-            }
-            if (getConfig().useBulk()) {
-                addBulkResult(caseId, duration, event.getResult());
-            } else {
-                send(caseId, duration, event.getResult());
-            }
-        } finally {
-            startTime.remove();
-        }
+        qaseReporter.onTestCaseFinished(getResultItem(event));
     }
 
-    private void addBulkResult(Long caseId, Duration duration, Result result) {
-        resultCreateBulk.addResultsItem(getResultItem(caseId, duration, result));
-    }
-
-    private ResultCreate getResultItem(Long caseId, Duration duration, Result result) {
-        StatusEnum status = convertStatus(result.getStatus());
-        Optional<Throwable> optionalThrowable = Optional.ofNullable(result.getError());
+    private ResultCreate getResultItem(TestCaseFinished event) {
+        List<String> tags = event.getTestCase().getTags();
+        Long caseId = CucumberUtils.getCaseId(tags);
+        StatusEnum status = convertStatus(event.getResult().getStatus());
+        Optional<Throwable> optionalThrowable = Optional.ofNullable(event.getResult().getError());
         String comment = optionalThrowable
-                .flatMap(throwable -> Optional.of(throwable.toString())).orElse(null);
+            .flatMap(throwable -> Optional.of(throwable.toString())).orElse(null);
         Boolean isDefect = optionalThrowable
-                .flatMap(throwable -> Optional.of(throwable instanceof AssertionError))
-                .orElse(false);
+            .flatMap(throwable -> Optional.of(throwable instanceof AssertionError))
+            .orElse(false);
         String stacktrace = optionalThrowable
-                .flatMap(throwable -> Optional.of(getStacktrace(throwable))).orElse(null);
+            .flatMap(throwable -> Optional.of(getStacktrace(throwable))).orElse(null);
         LinkedList<ResultCreateSteps> steps = StepStorage.getSteps();
         return new ResultCreate()
-                .caseId(caseId)
-                .status(status)
-                .timeMs(duration.toMillis())
-                .comment(comment)
-                .stacktrace(stacktrace)
-                .steps(steps.isEmpty() ? null : steps)
-                .defect(isDefect);
-    }
-
-    private void send(Long caseId, Duration duration, Result result) {
-        try {
-            resultsApi.createResult(getConfig().projectCode(),
-                    getConfig().runId(),
-                    getResultItem(caseId, duration, result));
-        } catch (QaseException e) {
-            logger.error(e.getMessage());
-        }
+            .caseId(caseId)
+            .status(status)
+            .comment(comment)
+            .stacktrace(stacktrace)
+            .steps(steps.isEmpty() ? null : steps)
+            .defect(isDefect);
     }
 
     private StatusEnum convertStatus(Status status) {
@@ -120,42 +84,6 @@ public class QaseEventListener implements ConcurrentEventListener {
             case UNUSED:
             default:
                 return StatusEnum.SKIPPED;
-        }
-    }
-
-    @Override
-    public void setEventPublisher(EventPublisher eventPublisher) {
-        if (QaseClient.isEnabled()) {
-            eventPublisher.registerHandlerFor(TestCaseStarted.class, this::testCaseStarted);
-            eventPublisher.registerHandlerFor(TestCaseFinished.class, this::testCaseFinished);
-            eventPublisher.registerHandlerFor(TestRunFinished.class, this::testRunFinished);
-        }
-    }
-
-    private void testRunFinished(TestRunFinished testRunFinished) {
-        if (getConfig().useBulk()) {
-            sendBulkResult();
-        }
-        if (getConfig().runAutocomplete()) {
-            try {
-                runsApi.completeRun(getConfig().projectCode(), getConfig().runId());
-            } catch (QaseException e) {
-                logger.error(e.getMessage());
-            }
-        }
-    }
-
-    private void sendBulkResult() {
-        try {
-            resultsApi.createResultBulk(
-                    getConfig().projectCode(),
-                    getConfig().runId(),
-                    resultCreateBulk
-            );
-            screenshotsSender.sendScreenshotsIfPermitted();
-            resultCreateBulk.getResults().clear();
-        } catch (QaseException e) {
-            logger.error(e.getMessage());
         }
     }
 }
