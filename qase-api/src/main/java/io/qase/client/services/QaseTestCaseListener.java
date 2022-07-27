e971ad62bad1a3322ee2bf96d9b17fb07eed1444
Mykhailo Birintsev <mykhailo.birintsev@gmail.com>
2022-07-27 21:03:41 +0300

    [~] - minor code styling

--- qase-api/src/main/java/io/qase/client/services/QaseTestCaseListener.java
+++ qase-api/src/main/java/io/qase/client/services/QaseTestCaseListener.java
@@ -3,6 +3,7 @@ package io.qase.client.services;
 import io.qase.client.model.ResultCreate;
 
 public interface QaseTestCaseListener {
+
     void reportResults(); // TODO: rename method to onTestCaseSetFinished
 
     void onTestCaseStarted();
