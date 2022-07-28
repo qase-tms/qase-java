e975f95190c4ff295bd6c0dadde5a967db46ea48
Mykhailo Birintsev <mykhailo.birintsev@gmail.com>
2022-07-29 00:05:36 +0300

    [+] - QaseConfigAutoReloadAspect

--- qase-api/src/main/java/io/qase/api/utils/TestUtils.java
+++ qase-api/src/main/java/io/qase/api/utils/TestUtils.java
@@ -1,7 +1,5 @@
 package io.qase.api.utils;
 
-import io.qase.api.QaseClient;
-
 import static io.qase.api.config.QaseConfig.*;
 
 /**
@@ -23,12 +21,10 @@ public final class TestUtils {
 
     public static void useBulk(boolean use) {
         System.setProperty(USE_BULK_KEY, String.valueOf(use));
-        QaseClient.getConfig().reload();
     }
 
     public static void useRunAutocomplete(boolean use) {
         System.setProperty(RUN_AUTOCOMPLETE_KEY, String.valueOf(use));
-        QaseClient.getConfig().reload();
     }
 
     public static void useScreenshotsSending(boolean use) {
@@ -36,7 +32,6 @@ public final class TestUtils {
         if (use) {
             System.setProperty(QASE_SCREENSHOT_FOLDER_KEY, "src/test/resources");
         }
-        QaseClient.getConfig().reload();
     }
 
     public static void setupQaseTestEnvironmentVariables(int testServerPort) {
