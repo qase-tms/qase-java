460c45e3c40ddf7fd891faebd7e457934237f52d
Mykhailo Birintsev <mykhailo.birintsev@gmail.com>
2022-08-01 12:05:06 +0300

    [~] - no-args constructor is generated by lombok, intervals between the constants were unified
    (cherry picked from commit 00585512a16e74eee9b2fbb9578125b92b729cda)

--- qase-api/src/main/java/io/qase/api/Constants.java
+++ qase-api/src/main/java/io/qase/api/Constants.java
@@ -1,11 +1,14 @@
 package io.qase.api;
 
-public final class Constants {
-    public static final String X_CLIENT_VERSION = "X-CLIENT-VERSION";
-    public static final String X_CLIENT_REPORTER = "X-CLIENT-REPORTER";
-    public static final String X_CLIENT_REPORTER_VERSION = "X-CLIENT-REPORTER-VERSION";
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
-    private Constants() throws IllegalAccessException {
-        throw new IllegalAccessException();
-    }
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constants {
+
+    public static final String X_CLIENT_VERSION = "X-CLIENT-VERSION";
+
+    public static final String X_CLIENT_REPORTER = "X-CLIENT-REPORTER";
+
+    public static final String X_CLIENT_REPORTER_VERSION = "X-CLIENT-REPORTER-VERSION";
 }
