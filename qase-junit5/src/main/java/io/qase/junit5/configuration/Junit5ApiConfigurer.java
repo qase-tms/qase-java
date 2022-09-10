f8b13620140113270f39d81dfd72f3daeb9622a4
Mykhailo Birintsev <m.birintsev@gmail.com>
2022-09-10 23:17:16 +0300

    [+] - ApiClientConfigurer was introduced for qase-junit5

---
+++ qase-junit5/src/main/java/io/qase/junit5/configuration/Junit5ApiConfigurer.java
@@ -0,0 +1,26 @@
+package io.qase.junit5.configuration;
+
+import com.google.inject.Singleton;
+import io.qase.api.config.apiclient.DefaultHeadersApiConfigurer;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+@Singleton
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class Junit5ApiConfigurer extends DefaultHeadersApiConfigurer {
+
+    private static final Class<?> JUNIT_FRAMEWORK_CLASS = Test.class;
+
+    private static final String JUNIT_FRAMEWORK_NAME = "junit5";
+
+    @Override
+    protected String getFrameworkName() {
+        return JUNIT_FRAMEWORK_NAME;
+    }
+
+    @Override
+    protected String getFrameworkVersion() {
+        return getFrameworkVersionByClassOrUnknown(JUNIT_FRAMEWORK_CLASS);
+    }
+}
