e34c3535b5e96692d3f5ed9c6d3c023e66c24d0a
Mykhailo Birintsev <m.birintsev@gmail.com>
2022-09-20 10:21:35 +0300

    [~] - code clean-up

--- qase-api/src/main/java/io/qase/guice/module/QaseModule.java
+++ qase-api/src/main/java/io/qase/guice/module/QaseModule.java
@@ -1,28 +1,23 @@
 package io.qase.guice.module;
 
 import com.google.inject.*;
-import io.qase.api.config.apiclient.ApiClientConfigurer;
 import io.qase.api.QaseClient;
-import io.qase.api.config.QaseConfig;
+import io.qase.api.config.apiclient.ApiClientConfigurer;
+import io.qase.api.services.QaseTestCaseListener;
+import io.qase.api.services.ReportersResultOperations;
 import io.qase.api.services.TestPlanService;
+import io.qase.api.services.impl.QaseTestCaseListenerImpl;
+import io.qase.api.services.impl.ReportersResultOperationsImpl;
 import io.qase.api.services.impl.TestPlanServiceImpl;
 import io.qase.client.ApiClient;
 import io.qase.client.api.AttachmentsApi;
 import io.qase.client.api.PlansApi;
 import io.qase.client.api.ResultsApi;
 import io.qase.client.api.RunsApi;
-import io.qase.api.services.QaseTestCaseListener;
-import io.qase.api.services.ReportersResultOperations;
-import io.qase.api.services.impl.QaseTestCaseListenerImpl;
-import io.qase.api.services.impl.ReportersResultOperationsImpl;
 import io.qase.plugin.codeparsing.ClassParser;
 import io.qase.plugin.codeparsing.MethodFilter;
 import io.qase.plugin.codeparsing.impl.ClassParserImpl;
 import io.qase.plugin.codeparsing.impl.MethodFilterImpl;
-import io.qase.client.ApiClient;
-import io.qase.client.api.AttachmentsApi;
-import io.qase.client.api.ResultsApi;
-import io.qase.client.api.RunsApi;
 
 public class QaseModule extends AbstractModule {
 
