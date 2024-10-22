fbeb2cb62cc8b74c0f390979a7cbd3a315c7c6af
Dmitrii Gridnev <dgridnev@qase.io>
2024-10-22 15:45:05 +0200

    feature: update cucumber 7

--- qase-cucumber-v7-reporter/src/main/java/io/qase/cucumber7/configuration/Cucumber7ApiConfigurer.java
+++ qase-cucumber-v7-reporter/src/main/java/io/qase/cucumber7/configuration/Cucumber7ApiConfigurer.java
@@ -1,26 +1,26 @@
-package io.qase.cucumber7.configuration;
-
-import com.google.inject.Singleton;
-import io.cucumber.java.After;
-import io.qase.config.DefaultHeadersApiConfigurer;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@Singleton
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
-public class Cucumber7ApiConfigurer extends DefaultHeadersApiConfigurer {
-
-    private static final Class<?> CUCUMBER_FRAMEWORK_CLASS = After.class;
-
-    private static final String CUCUMBER_FRAMEWORK_NAME = "cucumber7";
-
-    @Override
-    protected String getFrameworkName() {
-        return CUCUMBER_FRAMEWORK_NAME;
-    }
-
-    @Override
-    protected String getFrameworkVersion() {
-        return getFrameworkVersionByClassOrUnknown(CUCUMBER_FRAMEWORK_CLASS);
-    }
-}
+//package io.qase.cucumber7.configuration;
+//
+//import com.google.inject.Singleton;
+//import io.cucumber.java.After;
+//import io.qase.config.DefaultHeadersApiConfigurer;
+//import lombok.AccessLevel;
+//import lombok.NoArgsConstructor;
+//
+//@Singleton
+//@NoArgsConstructor(access = AccessLevel.PUBLIC)
+//public class Cucumber7ApiConfigurer extends DefaultHeadersApiConfigurer {
+//
+//    private static final Class<?> CUCUMBER_FRAMEWORK_CLASS = After.class;
+//
+//    private static final String CUCUMBER_FRAMEWORK_NAME = "cucumber7";
+//
+//    @Override
+//    protected String getFrameworkName() {
+//        return CUCUMBER_FRAMEWORK_NAME;
+//    }
+//
+//    @Override
+//    protected String getFrameworkVersion() {
+//        return getFrameworkVersionByClassOrUnknown(CUCUMBER_FRAMEWORK_CLASS);
+//    }
+//}
